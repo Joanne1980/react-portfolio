e03d344122667bfd3844f15aed7be5675dd55fc2
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-18 20:14:32 +0000

    About section completed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,27 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Contact from "../src/Pages/Contact";
-import Home from '../src/Pages/Home';
-import Projects from '../src/Pages/Projects';
-import Header from '../src/components/Header';
-
+import Home from "../src/Pages/Home";
+import Projects from "../src/Pages/Projects";
+import Header from "../src/components/Header";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <div>
-        {
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/projects/:id" element={<Projects />} />
-          </Routes>
-        }; 
-      </div>
-    </Router>
-  );
- 
-};
+    <>
+      <Router>
+        <Header />
+        <div>
+          {
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/projects/:id" element={<Projects />} />
+            </Routes>
+          }
+          ;
+        </div>
+      </Router>
 
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <Home />
+      </main>
+    </>
+  );
+}
 export default App;
