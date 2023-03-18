3bf8c97e074684f3d032d884e06e1bf7befacd39
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-18 20:49:38 +0000

    Added images to file

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-  return (
-    <section id='about'>
+  return (<>
+  <img className="object-cover object-center rounded"
+    alt="Photo of Joanne Clarke"
+    src="./sjc1.jpg"/>
+    <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center bg-blue-500">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mg-16 md:mb-0 items-center text-center">
-         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-          Hi, I'm Joanne.
-          <br className="hidden lg:inline-block"/> Client focused Web Developer.
-         </h1>
-         <p className="mb-8 leading-relaxed">
-          Enthusiastic front end web developer who has just completed a bootcamp with edX online learning
-          where I developed skills in HTML, CSS, JavaScript, React to name a few. Thriving on a challenge 
-          and is excited about the future and completing any projects. 
-         </p>
-        
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Joanne.
+            <br className="hidden lg:inline-block" /> Client focused Web
+            Developer.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Enthusiastic front end web developer who has just completed a
+            bootcamp with edX online learning where I developed skills in HTML,
+            CSS, JavaScript, React to name a few. Thriving on a challenge and is
+            excited about the future and completing any projects.
+          </p>
         </div>
       </div>
-  
-</section>
+    </section>
+    </>
   );
-  }
+}
